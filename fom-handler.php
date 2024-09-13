050b04ce89dc5a211488ed8099b3fbe8b874a87f
kevin chiloane <kndubane23@gmail.com>
2024-09-13 03:07:25 +0200

    initial commit

---
+++ fom-handler.php
@@ -0,0 +1,28 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject =$_POST['email_sub']
+$message = $_POST['message'];
+
+$email_from = 'rsftutorials.academy';
+$email_subject = 'New Form Submission'
+$email_body = "User Name: $name.\n"."User Email: $visitor_email.\n"."Subject: $subject.\n"."User Message: $message.\n";
+
+$to = 'rsfmanagement2@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply=TO: $visitor_email \r\n"
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("Location: Contact.html"); 
+
+
+
+
+
+
+
+
+?>
