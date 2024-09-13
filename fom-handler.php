e17fc00c337ac219ddf17e9785d754d750541a11
kevin chiloane <kndubane23@gmail.com>
2024-09-13 20:13:01 +0200

    php fix errors

--- fom-handler.php
+++ fom-handler.php
@@ -1,15 +1,22 @@
 <?php
 $name = $_POST['name'];
-$email = $_POST['email'];
+$visitor_email = $_POST['email'];
 $subject = $_POST['email_sub'];
 $message = $_POST['message'];
 
-$email_from = "From:".$name."<".$email.">\r\n";
+$email_from = 'rsftutorials.academy';
+$email_subject = 'New Form Submission';
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "Subject: $subject.\n".
+              "User Message: $message.\n";
 
-$to = 'kndubane23@gmail.com';
+$to = 'rsfmanagement2@gmail.com';
+//added
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
-mail($to, $email_subject, $message, $headers) or die("Error");
-echo"message send"; 
+mail($to, $email_subject, $email_body, $headers);
 
 // Redirect to the contact page after submission
 header("Location: Contact.html");
